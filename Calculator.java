b6a9c3cbb7f633cda871884da2e9fd5d888f9b1c
Eduardo Franshua Durand Obando <edurando@unsa.edu.pe>
2022-04-27 12:41:03 -0500

    Creando plantilla base

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator {
+	public static void main (String[] args) {
+		
+	}
+	
+	//METODOS
+
+	public static int add(int a, int b){
+		return 0;
+	}
+	public static int sub(int a, int b){
+		return 0;
+	}
+	public static int mul(int a, int b){
+		return 0;
+	}
+	public static int div(int a, int b){
+		return 0;
+	}
+	public static int mod(int a, int b){
+		return 0;
+	}
+}
